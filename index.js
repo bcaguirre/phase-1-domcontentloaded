c860e374bae8feb887f3d42942edaa724bccfe29
bernadette <aguirre.bernadette@outlook.com>
2022-04-26 14:25:47 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(document.querySelector('p'))
+    document.getElementById('text').textContent = 'This is really cool!';
+});
+
